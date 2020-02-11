dddced20c3841a5635cb8d0da90f3b374fb886ea
daphne Fang <daphneys.fang@gmail.com>
2020-02-11 00:42:21 -0800

    Updates

---
+++ precache-manifest.44804bc2738c5911a96ca8e891f55362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a7e63191a9bd7d30d699a31c5e367b",
+    "url": "/noteful/index.html"
+  },
+  {
+    "revision": "ddb045bb343fa87a9940",
+    "url": "/noteful/static/css/main.783aa64e.chunk.css"
+  },
+  {
+    "revision": "7c4e94fe22010703c177",
+    "url": "/noteful/static/js/2.30481947.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/noteful/static/js/2.30481947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddb045bb343fa87a9940",
+    "url": "/noteful/static/js/main.f9c2de92.chunk.js"
+  },
+  {
+    "revision": "4d1232d6749ce3abbc09",
+    "url": "/noteful/static/js/runtime-main.169eea1e.js"
+  }
+]);
