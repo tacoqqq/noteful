d9494473ebe34c929f37a520ab257c1c6f255401
daphne Fang <daphneys.fang@gmail.com>
2020-02-11 20:21:38 -0800

    fixed url

--- src/header/header.js
+++ src/header/header.js
@@ -6,7 +6,7 @@ function Header(){
     return(
         <header className='header'>
             <h1 className='header-title'>
-                <Link to="/">
+                <Link to={process.env.PUBLIC_URL + "/"}>
                     Noteful
                 </Link>
             </h1>
